c5c9ab2a0af3b0bb439cb5768bea83fdab274d0b
Davidgxn <david.gonalv11@gmail.com>
2023-03-25 00:33:25 +0100

    Cambiado el sistema de operaciones para que sea mas facil de implementar

--- index.js
+++ index.js
@@ -3,6 +3,9 @@
 // La variable output es el h2 que cuando se ejecuta la calculadora tiene un 0 y a medida que se van añadiendo numeros van saliendo en este output.
 const botones = document.getElementsByClassName("calculadora_boton")
 const output = document.getElementById("output");
+const operandoOutput = document.getElementById("operando");
+const operacionOutput = document.getElementById("operacion");
+let operando1, operacion;
 
 // Con un bucle for recorro todos los botones del array de botones y luego les añado un event listener para que cuando pulses sobre un boton ejecute la funcion.
 for (let i = 0; i < botones.length; i++) {
@@ -29,6 +32,9 @@ for (let i = 0; i < botones.length; i++) {
             case "reset":
                 // En el caso de pulsar el boton con la "C" se borra todo lo que esta en el output de la calculadora.
                 output.innerHTML = "0";
+                operandoOutput.innerHTML = "";
+                operacionOutput.innerHTML = "";
+                operando1 = null;
                 break;
             case "borrar":
                 // Cuando se pulsa el boton de borrar va eliminando el ultimo numero del output, y cuando no hay ningun numero en el output pone un 0 para que no se quede vacio el string.
@@ -37,133 +43,70 @@ for (let i = 0; i < botones.length; i++) {
                     output.innerHTML = "0";
                 }
                 break;
-            // Los botones de dividir, multiplicar, sumar y restar hacen practicamente lo mismo que es llamar a la funcion comprobarPosible pasando por parametros el signo que tienen que comprobar. En el caso de que se pueda poner lo ponen y en el de que no se pueda poner no se pone.
-            case "dividir":
-                comprobarPosible("/");
-                break;
-            case "multiplicar":
-                // Signo de multiplicacion en unicode de HTML &#215;
-                comprobarPosible("&#215;");
-                break;
             case "sumar":
-                comprobarPosible("+");
-                break;
             case "restar":
-                comprobarPosible("-");
+            case "multiplicar":
+            case "dividir":
+                añadirOperando(event.target.id);
                 break;
             case "resultado":
                 calcularResultado();
                 break;
             case "coma":
-                contenerPunto();
+                if (!output.innerHTML.includes(".")) {
+                    output.innerHTML += ".";
+                }
                 break;
         }
     });
 }
 
-function contenerPunto() {
-    // Esta funcion comprueba si es posible poner el punto y en caso de que si sea posible lo pone.
-    // En los primeros if else comprueba si hay un punto en el ultimo numero insertado entre un signo de puntuacion y 
-    if (output.innerHTML.includes("+")) {
-        let cadena = output.innerHTML.split("+");
-        if (!cadena[cadena.length - 1].includes(".")) {
-            output.innerHTML += ".";
-        }
-    } else if (output.innerHTML.includes("-")) {
-        let cadena = output.innerHTML.split("-");
-        if (!cadena[cadena.length - 1].includes(".")) {
-            output.innerHTML += ".";
-        }
-    } else if (output.innerHTML.includes("/")) {
-        let cadena = output.innerHTML.split("/");
-        if (!cadena[cadena.length - 1].includes(".")) {
-            output.innerHTML += ".";
-        }
-    } else if (output.innerHTML.includes("×")) {
-        let cadena = output.innerHTML.split("×");
-        if (!cadena[cadena.length - 1].includes(".")) {
-            output.innerHTML += ".";
-        }
-    } else {
-        if (!output.innerHTML.includes(".")) {
-            output.innerHTML += ".";
-        }
+function añadirOperando(tipoOperacion) {
+    operando1 = parseFloat(output.innerHTML);
+    operandoOutput.innerText = operando1;
+    operacion = tipoOperacion;
+    switch (tipoOperacion) {
+        case "sumar":
+            operacionOutput.innerHTML = "+";
+            break;
+        case "restar":
+            operacionOutput.innerHTML = "-";
+            break;
+        case "multiplicar":
+            operacionOutput.innerHTML = "&#215;";
+            break;
+        case "dividir":
+            operacionOutput.innerHTML = "/";
+            break;
     }
-}
-
-function comprobarPosible(signo) {
-    // Lo que hace esta funcion es comprobar si se puede poner el signo en el output
-    if (signo === "-") {
-        // Si se quiere poner el signo de menos lo que hace la funcion es comprobar si el ultimo caracter del output es un signo de suma o de resta, en el caso de que sea uno de estos lo sustituye por el signo negativo.
-        if (output.innerHTML[output.innerHTML.length - 1] === "-" || output.innerHTML[output.innerHTML.length - 1] === "+") {
-            output.innerHTML = output.innerHTML.slice(0, -1);
-            output.innerHTML += signo;
-        } else {
-            // En el caso de que no sea ninguno de estos puede poner el signo de restar.
-            output.innerHTML += signo;
-        }
-    } else {
-        // Cuando el signo no sea un signo de restar lo que hace es comprobar si el signo anterior es otro signo, que no sea el de restar, en caso de que sea otro signo que no sea el de restar lo sustituye por el signo que queremos poner.
-        if (output.innerHTML[output.innerHTML.length - 1] === "+" || output.innerHTML[output.innerHTML.length - 1] === "×" || output.innerHTML[output.innerHTML.length - 1] === "/") {
-            output.innerHTML = output.innerHTML.slice(0, -1);
-            output.innerHTML += signo;
-        } else if (output.innerHTML[output.innerHTML.length - 1] === "-") {
-            // Aqui ya sabemos que el ultimo caracter del output o no es ningun signo o es un signo de restar, en este caso comprobamos si es el de restar, y en caso de que sea comprobamos si el signo anterior al de restar es un signo de suma, multiplicacion o division, en este caso cambiamos los dos signos por el que queriamos.
-            if (output.innerHTML[output.innerHTML.length - 2] === "+" || output.innerHTML[output.innerHTML.length - 2] === "×" || output.innerHTML[output.innerHTML.length - 2] === "/") {
-                output.innerHTML = output.innerHTML.slice(0, -2);
-                output.innerHTML += signo;
-            } else {
-                // En el caso que el ultimo signo solo sea un signo de restar lo que hace es cambiar este signo por el que queremos poner.
-                output.innerHTML = output.innerHTML.slice(0, -1);
-                output.innerHTML += signo;
-            }
-        } else {
-            // En el caso de que no sea ningun signo lo que hace es añadirlo al final.
-            output.innerHTML += signo;
-        }
-    }
+    output.innerHTML = "0";
 }
 
 function calcularResultado() {
-    // Lo que hace esta funcion es separar la cadena en funcion de los elementos que la compongan para despues operar con ellos por separado y poder imprimir el resultado en el output.
-    let operacion = output.innerHTML;
-    let operandos;
-    let resultado;
-    if (operacion.includes("+")) {
-        // En el caso de que el output tenga un signo de suma divide la cadena por este signo para que luego sume todos los elementos por separado y despues lo muetra por el output.
-        operandos = operacion.split("+");
-        resultado = 0;
-        for (let i = 0; i<operandos.length; i++) {
-            resultado += parseFloat(operandos[i]);
+    if (operando1 != null) {
+        switch (operacion){
+            case "sumar":
+                output.innerText = operando1 + parseFloat(output.innerHTML);
+                operandoOutput.innerHTML = "";
+                operacionOutput.innerHTML = "";
+                break;
+            case "restar":
+                output.innerText = operando1 - parseFloat(output.innerHTML);
+                operandoOutput.innerHTML = "";
+                operacionOutput.innerHTML = "";
+                break;
+            case "multiplicar":
+                output.innerText = operando1 * parseFloat(output.innerHTML);
+                operandoOutput.innerHTML = "";
+                operacionOutput.innerHTML = "";
+                break;
+            case "dividir":
+                output.innerText = operando1 / parseFloat(output.innerHTML);
+                operandoOutput.innerHTML = "";
+                operacionOutput.innerHTML = "";
+                break;
         }
-        console.log(resultado);
-        output.innerHTML = resultado;
-    } else if (operacion.includes("-")) {
-        // En este caso hace lo mismo que antes pero dividiendo la cadena por el signo de restar y en vez de sumar los elementos, los resta.
-        operandos = operacion.split("-");
-        resultado = operandos[0];
-        for (let i = 1; i<operandos.length; i++) {
-            resultado -= parseFloat(operandos[i]);
-        }
-        console.log(resultado);
-        output.innerHTML = resultado;
-    } else if (operacion.includes("×")) {
-        // Otra vez lo mismo pero en este caso multiplicandom
-        operandos = operacion.split("×");
-        resultado = 1;
-        for (let i = 0; i<operandos.length; i++) {
-            resultado *= parseFloat(operandos[i]);
-        }
-        console.log(resultado);
-        output.innerHTML = resultado;
-    } else if (operacion.includes("/")) {
-        // En este caso hace lo mismo pero dividiendo.
-        operandos = operacion.split("/");
-        resultado = operandos[0];
-        for (let i = 1; i<operandos.length; i++) {
-            resultado /= parseFloat(operandos[i]);
-        }
-        console.log(resultado);
-        output.innerHTML = resultado;
+        operando1 = null;
+        operacion = null;
     }
 }
